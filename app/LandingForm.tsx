f8c889c8d12312b8fee2f72d1f7e54d8eb3cb6b0
Cedar-81 <cedarwudnovels@gmail.com>
2024-07-16 23:38:41 +0100

    updated whatsapp group link

--- app/LandingForm.tsx
+++ app/LandingForm.tsx
@@ -392,7 +392,7 @@ export default function LandingForm({ setShowForm }: Params) {
               className="p-2 border rounded-sm border-accent text-white w-full hover:animate-pulse"
               onClick={() => {
                 window.open(
-                  "https://chat.whatsapp.com/EfVpsGbeJgn3b3BL7Kqa9T",
+                  "https://chat.whatsapp.com/JObYiQa2uSD2I0cYNfbV1E",
                   "_blank"
                 );
                 setIsCommunityJoined(true);
