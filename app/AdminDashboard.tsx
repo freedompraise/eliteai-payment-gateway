dbb1f3116eaa595ce6c603c6d6ae2e19ac6f3ddd
Cedar-81 <cedarwudnovels@gmail.com>
2024-07-28 16:31:15 +0100

    modified admin dashboard table

--- app/AdminDashboard.tsx
+++ app/AdminDashboard.tsx
@@ -89,44 +89,71 @@ export default function AdminDashboard({
     { label: "full_name", renderCell: (item: any) => item.full_name },
     { label: "referrals", renderCell: (item: any) => item.referrals },
   ];
+  console.log(data);
 
   return (
-    <div className="h-screen w-screen fixed flex items-center justify-center top-0 right-0 bg-black z-40">
-      <div className=" min-h-[100vh] w-full text-white py-10 bg-black px-4 md:px-[80px]">
-        <div className="flex space-x-2 pb-10 items-center">
-          <button
-            onClick={() => {
-              setShowDashboard(false);
-              setAdminCode("");
-            }}
-          >
-            <ChevronLeft />
-          </button>
-          <h1 className="text-xl font-semibold text-white/70">Dashboard</h1>
-        </div>
-        <div className="p-4 bg-white/5 rounded border border-accent/10">
-          <div className="pb-4">
-            <h2 className="">Referrals</h2>
+    <div className="h-screen w-screen fixed overflow-auto top-0 right-0 bg-black z-40">
+      <div className="relative h-full w-full flex items-center justify-center py-10">
+        <div className=" h-full w-full text-white bg-black px-4 md:px-[80px]">
+          <div className="flex space-x-2 pb-10 items-center">
+            <button
+              onClick={() => {
+                setShowDashboard(false);
+                setAdminCode("");
+              }}
+            >
+              <ChevronLeft />
+            </button>
+            <h1 className="text-xl font-semibold text-white/70">Dashboard</h1>
           </div>
-
-          {isLoading && (
-            <h3 className="text-sm text-white/40">Loading data...</h3>
-          )}
-          {!isLoading && data && data.length > 0 && (
-            <div className="w-full border border-white/60 overflow-clip rounded !text-sm">
-              <CompactTable
-                columns={COLUMNS}
-                data={{ nodes: data }}
-                theme={theme}
-              />
+          <div className="p-4 bg-white/5 rounded border border-accent/10">
+            <div className="pb-4">
+              <h2 className="">Referrals</h2>
             </div>
-          )}
-          {(!isLoading && !data) ||
-            (!isLoading && data.length == 0 && (
-              <div className="w-3xl max-w-3xl border border-white/60 overflow-clip rounded !text-sm">
-                <h4 className="text-sm p-4">No one has made referrals yet</h4>
+
+            {isLoading && (
+              <h3 className="text-sm text-white/40">Loading data...</h3>
+            )}
+            {!isLoading && data && data.length > 0 && (
+              <div className="w-full border border-white/60 overflow-clip rounded !text-sm">
+                {/* <CompactTable
+                  columns={COLUMNS}
+                  data={{ nodes: data }}
+                  theme={theme}
+                /> */}
+
+                <div className="flex border-b border-b-white/60">
+                  <div className="w-1/2 border-r border-r-white/60 p-3">
+                    Full name
+                  </div>
+                  <div className="w-1/2 p-3">Referrals</div>
+                </div>
+
+                {data &&
+                  data.map((item, idx) => (
+                    <div className="flex border-b border-b-white/60" key={idx}>
+                      <div className="w-1/2 border-r border-r-white/60 p-3">
+                        {item.full_name}
+                      </div>
+                      <div className="w-1/2 p-3">
+                        {item.referrals}
+                        {item.referrals && (
+                          <span className="text-white/60 text-xs">
+                            {" " + "(" + item.referrals.length + ")"}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  ))}
               </div>
-            ))}
+            )}
+            {(!isLoading && !data) ||
+              (!isLoading && data.length == 0 && (
+                <div className="w-3xl max-w-3xl border border-white/60 overflow-clip rounded !text-sm">
+                  <h4 className="text-sm p-4">No one has made referrals yet</h4>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
